d7738842646da9a9a5d1f52f7f6ca19bffe84645
00437062 <Cui_BoWen@want-want.com>
2021-04-28 20:47:20 +0800

    2021-04-28: 搭建自定义页面

--- src/main/java/com/wen/spring_security/config/WebSecurityCorsConfig.java
+++ src/main/java/com/wen/spring_security/config/WebSecurityCorsConfig.java
@@ -3,8 +3,10 @@ package com.wen.spring_security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -34,10 +36,10 @@ public class WebSecurityCorsConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    /**
+ /*   *//**
      * 使用spring Security进行跨域配置
      * @return
-     */
+     *//*
     @Bean
     public  CorsConfigurationSource corsConfigurationSource(){
         //创建跨域配置容器
@@ -49,18 +51,47 @@ public class WebSecurityCorsConfig extends WebSecurityConfigurerAdapter {
         corsConfig.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
         source.registerCorsConfiguration("/*",corsConfig);
         return source;
+    }*/
+
+    /**
+     * 配置页面需要忽略的文件
+     * @param web
+     * @throws Exception
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/img/**/*", "/**/*.css", "/**/*.js","/template/**","/plugins/**");
     }
 
     /**
-     *
+     * 配置自定义登陆页面
      * @param http
      * @throws Exception
      */
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+    public void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage("/login.html")   //自定义登陆页面
+                .loginProcessingUrl("/login.do")  //配置登陆请求的URL
+                .defaultSuccessUrl("/pages/index.html")  //配置登陆成功的页面
+                .and()
+                .authorizeRequests().antMatchers("/pages/query.html").hasRole("ADMIN")
+                .and()
+                .authorizeRequests().antMatchers("/pages/**").authenticated()
+                .and()
+                .exceptionHandling().accessDeniedPage("/error.html") //配置登陆失败的界面
+                .and()
+                .csrf().disable(); //配置page文件夹下所有文件需要认证
     }
 
 
-
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //  基于内存  定义认证用户的账号 、密码 、以及权限关键字
+        auth.inMemoryAuthentication()
+                .withUser("root").password(passwordEncoder().encode("root")).roles("ROOT")
+                .and()
+                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
+    }
 }
